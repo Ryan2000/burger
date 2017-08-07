bc4f9a5d4e2373525f141cc3ab0ad7b4e782e710
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-08-06 18:35:25 -0700

    configured burgers controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,43 @@
 /**
  * Created by ryanhoyda on 8/6/17.
  */
+
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured", "date"
+    ], [
+        req.body.burger_name, false, new Date().toString()
+    ], function() {
+        res.redirect("/");
+    });
+});
+
+router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function() {
+        res.redirect("/");
+    });
+});
